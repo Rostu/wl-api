886f39cba73236f3f57cfb7f94e22d417e569317
Robert Studtrucker <robert.studtrucker@gmail.com>
2016-01-04 16:22:35 +0100

    first working test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var xmlparser = require('xml2js');
 
 var debug;
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
 var parseString = require('xml2js').parseString; // Parsing XML to JSON
 var soap = require('soap');
 
@@ -32,16 +32,10 @@ module.exports = {
                 console.log(err)
             }else {
                 console.log('Client created');
-                //console.log(client.describe());
-                //console.log(client); //all methods usable in the stub
-
                 client.execute(body, function(err, result) {
                     if(err){
                         console.log(err.body)
                     }else {
-                        //var test = JSON.stringify(result);
-                        //console.log(test);
-                        console.log(result.executeReturn.result.dataVectors[0].dataRow);
                         return result.executeReturn.result.dataVectors[0].dataRow;
                     }
                 });
@@ -53,11 +47,50 @@ module.exports = {
 
 
 
+//------------------------------------------------------------------------------------------------------------------------
 
 
+/*
+var url = 'http://wortschatz.uni-leipzig.de/axis/services/Baseform?wsdl';
+var options = {
+    wsdl: "Baseform" + '?wsdl',
+    namespaces: {
+        'xmlns:dat': 'http://datatypes.webservice.wortschatz.uni_leipzig.de',
+        'xmlns:urn': "Baseform"},
+    log: debug
+};
+var body = {
+    objRequestParameters: {
+        corpus: "de",
+        parameters: {
+            dataVectors: [{dataRow: ["Wort","gefahren"]}]
+        }
+    }
+};
+soap.createClient(url,options, function(err, client) {
+    client.setSecurity(new soap.BasicAuthSecurity('anonymous', 'anonymous'));
+    if(err){
+        console.log(err)
+    }else {
+        console.log('Client created');
+        //console.log(client.describe());
+        //console.log(client); //all methods usable in the stub
+
+        client.execute(body, function(err, result) {
+            if(err){
+                console.log(err.body)
+            }else {
+                //var test = JSON.stringify(result);
+                //console.log(test);
+                console.log(result.executeReturn.result.dataVectors[0].dataRow);
+                //return result.executeReturn.result.dataVectors[0].dataRow;
+            }
+        });
+    }
+});
 
 
-
+*/
 
 
 
--- tests/index.js
+++ tests/index.js
@@ -3,3 +3,19 @@ var wlapi = require('../index');
 var baseform = wlapi.baseform;
 
 //Tests will follow
+
+describe('#baseform', function() {
+    it('gets a String and returns an Array', function () {
+        baseform('Test').constructor.should.equal(Array);
+    });
+
+    it('returns the baseform of a german word', function () {
+        baseform('gefahren')[0].should.equal("fahren");
+    });
+
+    it('returns the wordform of a given word', function () {
+        baseform('gefahren')[1].should.equal("V");
+    });
+
+
+});
