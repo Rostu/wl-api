b68b7a3138d978a0ea0ac3ae3592d85d462978cd
Robert Studtrucker <robert.studtrucker@gmail.com>
2016-01-08 17:40:45 +0100

    rough work done

--- index.js
+++ index.js
@@ -3,22 +3,28 @@
 //var async = require('async');
 //var parseString = require('xml2js').parseString; // Parsing XML to JSON
 var soap = require('soap');
+var underscore = require("underscore");
 
 // Call back
 
 module.exports = {
+    //Should returns the lemmatized (base) form of the input word.
+    //returns array of possible baseforms in some cases
+    //wlapi.baseform("")-->
     baseform: function(word) {
-        //check if one string or array of strings
         var arg = [{dataRow: ["Wort", word]}];
         query("Baseform",arg, function(err,callback){
             return create_results(callback);
         });
     },
+    //Returns the frequency and frequency class of the input word.
+    //Frequency class is computed in relation to the most frequent word in the corpus.
+    // The higher the class, the rarer the word.
+    //wlapi.frequencies("Esel")-->[ '3524', '12' ]
     frequencies: function(word) {
-        //check if one string or array of strings
         var arg = [{dataRow: ["Wort", word]}];
         query("Frequencies",arg, function(err,callback){
-            return create_results(callback);
+            return underscore.flatten(create_results(callback));
         });
     },
     //Returns categories for a given input word as an array:
@@ -28,34 +34,125 @@ module.exports = {
             return create_results(callback);
         });
     },
+    //For a given word form returns all other word forms of the same lemma
     wordforms: function(word,limit) {
         if(!limit){limit = 10;}
         var arg = [{dataRow: ["Word", word]},{dataRow: ["Limit", limit]}];
         query("Wordforms",arg, function(err,callback){
             return create_results(callback);
         });
+    },
+    thesaurus: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("Thesaurus",arg, function(err,callback){
+            return underscore.flatten(create_results(callback));
+        });
+    },
+    synonyms: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("Synonyms",arg, function(err,callback){
+            return create_results(callback);
+        });
 
     },
-    baseform: function(word) {
+    sentence: function(word,limit) {
+        if(!limit){limit = 5;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("Sentences",arg, function(err,callback){
+            return create_results(callback);
+        });
+    },
+    left_neighbours: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("LeftNeighbours",arg, function(err,callback){
+            return create_results(callback);
+        });
 
     },
-    baseform: function(word) {
-
+    right_neighbours: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("RightNeighbours",arg, function(err,callback){
+            return create_results(callback);
+        });
     },
-    baseform: function(word) {
-
+    similarity: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("Similarity",arg, function(err,callback){
+            return create_results(callback);
+        });
     },
-    baseform: function(word) {
-
+    experimental_synonyms: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("ExperimentalSynonyms",arg, function(err,callback){
+            return create_results(callback);
+        });
     },
-    baseform: function(word) {
-
+    /*not working so far----------------------------
+    ngrams: function(pattern,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Pattern", pattern]},{dataRow: ["Limit", limit]}];
+        query("NGrams",arg, function(err,callback){
+            return create_results(callback);
+        });
     },
-    baseform: function(word) {
-
+    ngram_references: function(word,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Limit", limit]}];
+        query("NGramReferences",arg, function(err,callback){
+            return create_results(callback);
+        });
     },
-    baseform: function(word) {
-
+    */
+    right_collocation_finder: function(word,postag,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Wortart", postag]},{dataRow: ["Limit", limit]}];
+        query("RightCollocationFinder",arg, function(err,callback){
+            return create_results(callback);
+        });
+    },
+    left_collocation_finder: function(word,postag,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Wortart", postag]},{dataRow: ["Limit", limit]}];
+        query("LeftCollocationFinder",arg, function(err,callback){
+            return create_results(callback);
+        });
+    },
+    cooccurrences: function(word,sign,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Mindestsignifikanz", sign]},{dataRow: ["Limit", limit]}];
+        query("Cooccurrences",arg, function(err,callback){
+            return create_results(callback);
+        });
+    },
+    //to use this service you have to have a user account different from anonymous
+    cooccurrences_all: function(word,sign,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ["Mindestsignifikanz", sign]},{dataRow: ["Limit", limit]}];
+        query("CooccurrencesAll",arg, function(err,callback){
+            return create_results(callback);
+        });
+    },
+    //to use this service you have to have a user account different from anonymous
+    intersection: function(word1,word2,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort 1", word1]},{dataRow: ["Wort 2", word2]},{dataRow: ["Limit", limit]}];
+        query("Kookkurrenzschnitt",arg, function(err,callback){
+            return create_results(callback);
+        });
+    },
+    //Takes some time
+    crossword: function(word, word_length,limit) {
+        if(!limit){limit = 10;}
+        var arg = [{dataRow: ["Wort", word]},{dataRow: ['Wortlaenge', word_length]},{dataRow: ["Limit", limit]}];
+        query("Kreuzwortraetsel",arg, function(err,callback){
+            return create_results(callback);
+        });
     }
 
 };
@@ -95,16 +192,17 @@ function query(service,arg,cb) {
     soap.createClient(url,options, function(err, client) {
         client.setSecurity(new soap.BasicAuthSecurity('anonymous', 'anonymous'));
         if(err){
-            console.log(err);
+            console.log("Client create Error: "+err);
             return (err);
         }else {
             console.log("Client created");
             client.execute(body, function(err, result) {
                 if(err){
-                    console.log(err.body);
+                    console.log("query Error: "+err.body);
                     return (err.body);
                 }else {
                     if(result.executeReturn.result.dataVectors){
+                        //console.log(result.executeReturn.result);
                         cb(null,result.executeReturn.result.dataVectors);
                     }else{cb(null,null);}
                 }
@@ -141,9 +239,17 @@ function create_results(res){
 //------------------------------------------------------------------------------------------------------------------------
 //test
 
-var testarg = [{dataRow: ["Word", "laufen"]},{dataRow: ["Limit", 20]}];
+var testarg = [{dataRow: ["Pattern", "Esel"]},{dataRow: ["Limit", 5]}];
+var testarg2 = [{dataRow: ["Wort", "Esel"]}];
+var testarg3 = [{dataRow: ["Wort", "Esel"]},{dataRow: ["Wortart", "A"]},{dataRow: ["Limit", 10]}];
+var coocurencearg = [{dataRow: ["Wort", "Esel"]},{dataRow: ["Mindestsignifikanz", 200]},{dataRow: ["Limit", 10]}];
+var Kookurrenzschnitt = [{dataRow: ["Wort 1", "Esel"]},{dataRow: ["Wort 2", "Karren"]},{dataRow: ["Limit", 5]}];
+var crossword = [{dataRow: ["Wort", "Flug"]},{dataRow: ['Wortlaenge', 6]},{dataRow: ["Limit", 5]}];
 
-query("Wordforms",testarg, function(err, callback){
+
+query("Frequencies",testarg2, function(err, callback){
+    //console.log(callback);
+    //console.log(underscore.flatten(create_results(callback)));
     console.log(create_results(callback));
 });
 //console.log(client.describe());
